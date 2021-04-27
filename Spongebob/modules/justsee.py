2f976a8304047e634fd67b56f8d292941ca6cf0c
SpongebobSquarepantsKrustyKrab <82528174+SpongebobSquarepantsKrustyKrab@users.noreply.github.com>
2021-04-27 14:55:36 +0700

    Add files via upload

---
+++ Spongebob/modules/justsee.py
@@ -0,0 +1,11 @@
+__help__ = f"""
+*Available Commands* *:*
+How To Play Music In Voice Chat?
+Add @spongebobgroupsmusicbot and @spongebobmusic then add him admin
+• `/play` *:* To play a song
+• `/song` *:* search a song
+ 
+Report bugs at @VohaUnion.
+"""
+
+__mod_name__ = "Voice Chat"
---
+++ Spongebob/modules/owo.py
@@ -0,0 +1,9 @@
+__help__ = f"""
+*Available Commands* *:*
+
+*Telegraph Media*:
+• `/tm` *:* reply to photo/video/gif
+
+"""
+
+__mod_name__ = "Telegraph"
