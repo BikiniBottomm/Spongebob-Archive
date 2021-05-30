1441b897c8826b452ef2a2a2da7b8fca3653b533
ilhambumulo <84878662+ilhambumulo@users.noreply.github.com>
2021-05-30 23:15:04 +0800

    Update filters.py

--- Spongebob/modules/helper_funcs/filters.py
+++ Spongebob/modules/helper_funcs/filters.py
@@ -4,7 +4,14 @@ from telegram.ext import BaseFilter
 
 
 class CustomFilters(object):
-    
+
+    class _Supporters(BaseFilter):
+
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DEMONS)
+
+    support_filter = _Supporters()
+
     class _Sudoers(BaseFilter):
 
         def filter(self, message: Message):
