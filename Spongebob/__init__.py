b0709a4ea17deb9c8d2bb759b3d041f5a15e11f4
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-06-02 20:17:07 +0700

    Update __init__.py

--- Spongebob/__init__.py
+++ Spongebob/__init__.py
@@ -191,14 +191,9 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("KrustyKrab", API_ID, API_HASH)
-pbot = Client("Spongebob", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("SpongebobPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 tbot = telethn
-
-kp = Client("SpongebobPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
-apps = []
-apps.append(kp)
-
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
